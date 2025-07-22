bb1c8d6350f7f1f2ec94cab1f12b5c2ac0f40c38
ussy <usmanhassanish@gmail.com>
2025-07-22 16:33:41 +0100

    update

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('menu-item').addEventListener('click', function() {
-  document.getElementById('popup').style.display = 'block';
-});
-
-document.getElementById('close-btn').addEventListener('click', function() {
-  document.getElementById('popup').style.display = 'none';
-});
